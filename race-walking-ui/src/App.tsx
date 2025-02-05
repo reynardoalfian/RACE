5881e22a61d33efcc45878080878038a0c341311
Reynardo Alfian <reynardoalfian55@gmail.com>
2025-02-05 14:35:19 -0500

    App.tsx

--- race-walking-ui/src/App.tsx
+++ race-walking-ui/src/App.tsx
@@ -1,27 +1,25 @@
-// src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Auth from './pages/Auth'
-import About from './pages/About'
-import './index.css'; 
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Auth from './pages/Auth';
+import About from './pages/About';
+import './index.css';
+
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  )
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
